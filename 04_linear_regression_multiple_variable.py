d2bc6da5fcbcb1f049543ddc34c7779de9d8c0c5
Wonhee Jung <wonhee.jung@gmail.com>
2017-03-24 03:30:33 -0700

    Update comment

--- 04_linear_regression_multiple_variable.py
+++ 04_linear_regression_multiple_variable.py
@@ -6,7 +6,7 @@ import tensorflow as tf
 
 num_points = 22 
 vectors_set = []
-# x : house price, y: # of bedrooms, z : sqft
+# x : sqft, y: # of bedrooms, z : price
 vectors_set.append([1250, 2, 580000])
 vectors_set.append([1230, 2, 540000])
 vectors_set.append([1300, 3, 600000])
